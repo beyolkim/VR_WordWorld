59babf50273fc7c7d1b558be43597cbcad2a246e
veroxxx <54518869+veroxxx@users.noreply.github.com>
2019-09-04 18:23:30 +0900

    스타킴

--- VR_WordWorld/Assets/02.Scripts/ObjectController.cs
+++ VR_WordWorld/Assets/02.Scripts/ObjectController.cs
@@ -1,34 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class ObjectController : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+public class ObjectController : MonoBehaviour
 {
     Transform tr;
     private LaserCaster laserCaster;
-    private GameObject rController;
-    private GameObject particle;
-    private AudioSource _audio;
-    public AudioClip enterSfx;
-    public AudioClip clickSfx;
-
-    private Renderer rend;
-    private Color NormalColor;
-    public Color HighlightColor;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        _audio = this.gameObject.AddComponent<AudioSource>();
         tr = GetComponent<Transform>();
-        rend = GetComponent<Renderer>();
-        NormalColor = GetComponent<Renderer>().material.color;
-
         laserCaster = GameObject.FindGameObjectWithTag("RController").GetComponent<LaserCaster>();
-        rController = GameObject.FindGameObjectWithTag("RController");
-        particle = GameObject.FindGameObjectWithTag("Particles");
     }
 
     void FixedUpdate()
@@ -37,36 +19,5 @@ public class ObjectController : MonoBehaviour, IPointerEnterHandler, IPointerCli
         {
             laserCaster.Drop();
         }
-        if (tr.position.y >= 8f) {
-            laserCaster.Drop();
-        }
-        // Mission();
-    }
-    void Mission()
-    {
-        Vector3 pos = tr.position;
-        if (pos.y > 8f && !tr.IsChildOf(rController.transform))
-        {            
-                Debug.Log("참 잘했어요!!");                 
-                particle.GetComponent<ParticleSystem>().Play(); 
-                //미션 성공 파티클
-            
-        }
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        _audio.PlayOneShot(enterSfx);
-        rend.material.color = HighlightColor;
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        _audio.PlayOneShot(clickSfx);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        rend.material.color = NormalColor;
-    }
+    } 
 }
