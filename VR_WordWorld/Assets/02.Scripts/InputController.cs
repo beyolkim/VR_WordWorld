bd3cc04e4f475239e16f9b9450a4fd2ba5004653
veroxxx <54518869+veroxxx@users.noreply.github.com>
2019-09-04 17:26:51 +0900

    15 별님

--- VR_WordWorld/Assets/02.Scripts/InputController.cs
+++ VR_WordWorld/Assets/02.Scripts/InputController.cs
@@ -8,20 +8,15 @@ public class InputController : MonoBehaviour
 {
 
     //Move 변수들
-    public float Speed = 6;
+    public float Speed = 12;
     Transform tr;
     private Transform cam;
-
-    private Rigidbody rb;
-    private CharacterController cc;
     private GameObject Birth_Text;
     private GameObject voice_text;
-    bool check_text = false;
-
+    bool check_text=false;
     //녹화 버튼 활성화 변수들
     public IBM.Watsson.Examples.ExampleStreaming voice;
     bool isWastssonEnable = false;
-    
     //임시 녹음버튼
     public GameObject test_Record_Image;
     public GameObject introUI;
@@ -29,60 +24,53 @@ public class InputController : MonoBehaviour
 
     private Hangle break_word;
 
-    //LaserCaster 사용 스크립트 및 변수
-
-    private bool breaking = false;
-    private LaserCaster break_laser;
-    public GameObject Laser_Script;
+ 
 
     //public static GameObject voice_text;
     void Start()
     {
-        tr = GetComponent<Transform>();
-        rb = GetComponent<Rigidbody>();
-        cc = GetComponent<CharacterController>();
+        tr = GetComponent<Transform>(); 
         cam = tr.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor").GetComponent<Transform>();
         Birth_Text = Resources.Load<GameObject>("3D_TEXT");
-
-        break_laser = GameObject.FindGameObjectWithTag("RController").GetComponent<LaserCaster>();
-
-
+ 
     }
 
     void Update()
     {
 
         //녹화버튼 On/Off
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad) /*|| Input.GetMouseButtonDown(0)*/)
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad) || Input.GetMouseButtonDown(0))
         {
             Debug.Log("Button Cliked !!!");
             if (isWastssonEnable == false)
             {
                 Debug.Log("OnOnOn!!!");
-                breaking = false;
+
                 Vector3 _dir = new Vector3(0, 0, 20); // vec를 vec3값으로 변환(_dir은 로컬 좌표) 
-                Vector3 voice_dir = new Vector3(0, 0, 19);
+                Vector3 voice_dir = new Vector3(0, 0, 19); 
 
                 if (check_text == false)
                 {
                     check_text = !check_text;
                     Debug.Log("3dtext가 만들어지고 있니?");
                     voice_text = Instantiate(Birth_Text);
-
+                    
                     voice_text.transform.SetParent(cam);
                     voice_text.transform.localPosition = voice_dir;
                     voice_text.transform.localRotation = Quaternion.identity;
-               
+                    //voice_text.GetComponent<TextMesh>().text = voice.text_3D.text;
                     voice_text.GetComponent<TextMesh>().text = voice.ResultsField.text;
-                    
+
+
                 }
 
                 test_Record_Image.SetActive(true);    // test_Record_Image 활성화
-
+                
                 test_Record_Image.transform.SetParent(cam);  //test_Record_Image 위치 카메라 위치로 수정
                 test_Record_Image.transform.localPosition = _dir;
                 test_Record_Image.transform.localRotation = Quaternion.identity;
-                
+
+
                 isWastssonEnable = !isWastssonEnable;
                 voice.Active = true;
 
@@ -92,82 +80,56 @@ public class InputController : MonoBehaviour
             {
                 Debug.Log("OffOffOff!!!");
                 isWastssonEnable = !isWastssonEnable;
-                breaking = true;
                 voice.Active = false;
                 test_Record_Image.SetActive(false);
 
             }
 
         }
-
-        //트랙패드 터치 좌표값
-
         if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad))
         {
+            //트랙패드 터치 좌표값
             Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
-            // Debug.LogFormat("Touch Postion x={0}, y={1}", pos.x, pos.y);
+            Debug.LogFormat("Touch Postion x={0}, y={1}", pos.x, pos.y);
 
         }
-
-        //보이스 레코드 갱신화
-        if (check_text == true)
-
+        if(check_text == true) // 보이스 레코드 갱신화
         {
             voice_text.GetComponent<TextMesh>().text = voice.ResultsField.text;
         }
-        
- 
-        // 트리거 버튼을 이용하 글자 블록 부시기
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && breaking == true /*|| Input.GetMouseButtonDown(1)*/)
+
+        if (/*OVRInput.Get(OVRInput.Button.Back) ||*/ Input.GetMouseButtonDown(1))
         {
+            
+            Debug.Log("Word_break");
+            break_word = voice_text.gameObject.AddComponent<Hangle>();
+            break_word.check_break = false;
 
-            Debug.Log("1st_breakbreakbreak");
-            Debug.Log(tr.position);
-            break_laser.P_ray = new Ray(break_laser.tr.position, break_laser.tr.forward);
-            Debug.Log("2st_breakbreakbreak");
-            if (Physics.Raycast(break_laser.P_ray, out break_laser.hit, break_laser.range))
-            {
-                if (break_laser.hit.collider.CompareTag("RecordingText"))
-                {
-                    Debug.Log("breakbreakbreak");
-                    Break_Word();
-                }
-
-            }
-
-
+            
+            check_text = !check_text;
+            voice.ResultsField.text = "----------";
+            Destroy(voice_text,0.5f);
         }
 
         MovePlayer();
     }
-
-    void Break_Word()
-    {
-        break_word = voice_text.gameObject.AddComponent<Hangle>();
-        break_word.check_break = false;
-
-        check_text = !check_text;
-        voice.ResultsField.text = "----------";
-        Destroy(voice_text, 0.2f);
-
-    }
     void MovePlayer()
     {
-        Vector2 vec = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
-        Vector3 _dir = new Vector3(vec.x, 0, vec.y); // vec를 vec3값으로 변환(_dir은 로컬 좌표) 
-        Vector3 dir = cam.transform.TransformDirection(_dir); // 카메라의 vector, _dir을 월드좌표로
 
         if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad))
         {
-            if (dir.y < 0 && transform.position.y < 0f)
+            Vector2 vec = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+            Vector3 _dir = new Vector3(vec.x, 0, vec.y); // vec를 vec3값으로 변환(_dir은 로컬 좌표) 
+            Vector3 dir = cam.transform.TransformDirection(_dir); // 카메라의 vector, _dir을 월드좌표로
+
+            if (dir.y < 0 && transform.position.y < 0)
             {
+
                 dir.y = 0f;
             }
-            
-            cc.Move(dir * Speed * Time.deltaTime);
 
-            // transform.Translate(dir * Speed * Time.deltaTime); // player 이동
-        }        
-    }   
+            transform.Translate(dir * Speed * Time.deltaTime); // player 이동
+        }
+    }
 
 }
